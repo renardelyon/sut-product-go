8ae5466058125a9dd566f03bcfe449e80172c872
renardelyon <renard.elyon.r@gmail.com>
2022-07-10 23:06:55 +0700

    fix saving to user product table logic

--- domain/product/service/save_requested_gift.go
+++ domain/product/service/save_requested_gift.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"sut-product-go/domain/product/model"
 	productpb "sut-product-go/pb/product"
@@ -30,21 +32,45 @@ func (s *Service) SaveRequestedGift(ctx context.Context, reqSave *productpb.Save
 		}
 	}
 
-	res := s.H.DB.Create(&model.UserProduct{
-		AdminId:     reqSave.AdminId,
-		Fullname:    reqSave.Fullname,
-		Username:    reqSave.Username,
-		UserId:      reqSave.UserId,
-		ProductId:   productId,
-		RequestDate: timeNow,
-		Status:      model.Pending,
-	})
+	var products []model.Product
 
-	if res.Error != nil {
+	query := fmt.Sprintf(`
+		select *
+		from user_products up
+		where product_id  in
+		(select p.id
+		from user_products up
+		inner join products p
+		on up.product_id = p.id
+		where p."name" = '%s')
+		and user_id = '%s'
+	`, reqSave.Productname, reqSave.UserId)
+
+	if res := s.H.DB.Raw(query).Scan(&products); res.Error != nil {
+		log.Println(res.Error)
 		return &productpb.SaveRequestedGiftResponse{
 			Status: http.StatusInternalServerError,
 			Error:  res.Error.Error(),
 		}, nil
+	}
+
+	if len(products) < 1 {
+		res := s.H.DB.Create(&model.UserProduct{
+			AdminId:     reqSave.AdminId,
+			Fullname:    reqSave.Fullname,
+			Username:    reqSave.Username,
+			UserId:      reqSave.UserId,
+			ProductId:   productId,
+			RequestDate: timeNow,
+			Status:      model.Pending,
+		})
+
+		if res.Error != nil {
+			return &productpb.SaveRequestedGiftResponse{
+				Status: http.StatusInternalServerError,
+				Error:  res.Error.Error(),
+			}, nil
+		}
 	}
 
 	return &productpb.SaveRequestedGiftResponse{
