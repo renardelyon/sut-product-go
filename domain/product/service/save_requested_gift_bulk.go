92b5132fe5a3648fa497ba5aecbb451583007df2
renardelyon <renard.elyon.r@gmail.com>
2022-07-11 07:15:34 +0700

    fix save requested bulk cannot save data to db

--- domain/product/service/save_requested_gift_bulk.go
+++ domain/product/service/save_requested_gift_bulk.go
@@ -7,14 +7,28 @@ import (
 	productpb "sut-product-go/pb/product"
 )
 
+type Temp struct {
+	AdminId  string
+	Fullname string
+	Username string
+	UserId   string
+}
+
 func (s *Service) SaveRequestedGiftBulk(ctx context.Context, reqSave *productpb.SaveRequestedGiftBulkRequest) (*productpb.SaveRequestedGiftBulkResponse, error) {
+	temp := Temp{
+		AdminId:  reqSave.AdminId,
+		Fullname: reqSave.Fullname,
+		Username: reqSave.Username,
+		UserId:   reqSave.UserId,
+	}
+
 	products := make([]model.Product, len(reqSave.ProductInfo))
 
 	chanIndex := s.generateProductIndexes(reqSave.ProductInfo)
 
 	chanProductId := s.insertProductInfoIntoProductTable(chanIndex, products, 50)
 
-	s.insertProductIdIntoUserProductTable(chanProductId, reqSave)
+	s.insertProductIdIntoUserProductTable(chanProductId, temp)
 
 	return &productpb.SaveRequestedGiftBulkResponse{
 		Status: http.StatusOK,
--- domain/product/service/util.go
+++ domain/product/service/util.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"log"
 	"sut-product-go/domain/product/model"
 	productpb "sut-product-go/pb/product"
@@ -15,6 +16,8 @@ type ProductInfo struct {
 	name  string
 	url   string
 }
+
+type productIdAndName map[string]string
 
 func (s *Service) generateProductIndexes(productInfos []*productpb.ProductInfo) <-chan ProductInfo {
 	chanOut := make(chan ProductInfo)
@@ -33,8 +36,9 @@ func (s *Service) generateProductIndexes(productInfos []*productpb.ProductInfo) 
 	return chanOut
 }
 
-func (s *Service) insertProductInfoIntoProductTable(chanIn <-chan ProductInfo, products []model.Product, numOfWorkers int) <-chan string {
-	chanOut := make(chan string)
+func (s *Service) insertProductInfoIntoProductTable(chanIn <-chan ProductInfo, products []model.Product, numOfWorkers int) <-chan productIdAndName {
+	chanOut := make(chan productIdAndName)
+	chanIsDataInDB := make(chan bool)
 	var product model.Product
 	var wg sync.WaitGroup
 
@@ -51,7 +55,17 @@ func (s *Service) insertProductInfoIntoProductTable(chanIn <-chan ProductInfo, p
 							Name: productInfo.name,
 							Url:  productInfo.url,
 						}
-						chanOut <- productId
+						chanOut <- productIdAndName{
+							"productId":   productId,
+							"productName": productInfo.name,
+						}
+						chanIsDataInDB <- false
+					} else {
+						chanOut <- productIdAndName{
+							"productId":   product.Id,
+							"productName": product.Name,
+						}
+						chanIsDataInDB <- true
 					}
 				}
 
@@ -61,42 +75,80 @@ func (s *Service) insertProductInfoIntoProductTable(chanIn <-chan ProductInfo, p
 	}()
 
 	go func() {
+		var dataInDBQty int
+
+		for isDataInDb := range chanIsDataInDB {
+			if isDataInDb {
+				dataInDBQty++
+			}
+		}
+
+		if dataInDBQty <= 0 {
+			res := s.H.DB.Create(&products)
+			if res.Error != nil {
+				log.Println(res.Error)
+			}
+		}
+	}()
+
+	go func() {
 		wg.Wait()
 		close(chanOut)
-		res := s.H.DB.Create(&products)
-		if res.Error != nil {
-			log.Fatalln(res.Error)
-		}
+		close(chanIsDataInDB)
 	}()
 
 	return chanOut
 }
 
-func (s *Service) insertProductIdIntoUserProductTable(chanIn <-chan string, reqSave *productpb.SaveRequestedGiftBulkRequest) {
+func (s *Service) insertProductIdIntoUserProductTable(chanIn <-chan productIdAndName, tempProductInfo Temp) {
 	var userProducts = make([]model.UserProduct, 0)
-	var chanOut = make(chan []model.UserProduct)
 	timeNow := time.Now().Local().UTC().String()
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
 	go func() {
-		for productId := range chanIn {
-			userProducts = append(userProducts, model.UserProduct{
-				AdminId:     reqSave.AdminId,
-				Fullname:    reqSave.Fullname,
-				Username:    reqSave.Username,
-				UserId:      reqSave.UserId,
-				ProductId:   productId,
-				RequestDate: timeNow,
-				Status:      model.Pending,
-			})
+		for productIdAndName := range chanIn {
+			var products []model.Product
+
+			query := fmt.Sprintf(`
+				select *
+				from user_products up
+				where product_id  in
+				(select p.id
+				from user_products up
+				inner join products p
+				on up.product_id = p.id
+				where p."name" = '%s')
+				and user_id = '%s'
+			`, productIdAndName["productName"], tempProductInfo.UserId)
+
+			if res := s.H.DB.Raw(query).Scan(&products); res.Error != nil {
+				log.Println(res.Error)
+			}
+
+			if len(products) < 1 {
+				userProducts = append(userProducts, model.UserProduct{
+					AdminId:     tempProductInfo.AdminId,
+					Fullname:    tempProductInfo.Fullname,
+					Username:    tempProductInfo.Username,
+					UserId:      tempProductInfo.UserId,
+					ProductId:   productIdAndName["productId"],
+					RequestDate: timeNow,
+					Status:      model.Pending,
+				})
+			}
 		}
 
-		chanOut <- userProducts
-		close(chanOut)
+		wg.Done()
 	}()
 
-	up := <-chanOut
-	res := s.H.DB.Create(&up)
-	if res.Error != nil {
-		log.Fatalln(res.Error)
+	wg.Wait()
+	if len(userProducts) > 0 {
+		res := s.H.DB.Create(&userProducts)
+		if res.Error != nil {
+			log.Fatalln(res.Error)
+		}
 	}
 }
